fde354c2217171db4febdab68f3ba6e8aa2301b8
Victor <vbivol394@gmail.com>
2025-11-30 10:29:42 +0200

    am facut ceva

--- 13Lesson.py
+++ 13Lesson.py
@@ -3,16 +3,18 @@ import cv2
 def noth(x):
     pass
 
-img = cv2.imread("pacman.png")
-cv2.namedWindow("track",cv2.WINDOW_NORMAL)
-cv2.resizeWindow("track",400,400)
-cv2.createTrackbar("sigma","track",1,100,noth)
+
+img = cv2.imread('python.png')
+
+cv2.namedWindow('ceva',cv2.WINDOW_NORMAL)
+cv2.resizeWindow("ceva",400,400)
+cv2.createTrackbar('sigma','ceva',1,100,noth)
 
 while True:
-    sigma = cv2.getTrackbarPos('sigma','track')
-    blur = cv2.GaussianBlur(img,(9,9),sigma)
-    canny = cv2.Canny(blur,10,200)
-    cv2.imshow("track",canny)
+    sigma = cv2.getTrackbarPos('sigma','ceva')
+    blur = cv2.GaussianBlur(img,(21,21),sigma)
+    canny = cv2.Canny(blur,100,200)
+    cv2.imshow('ceva',canny)
     cv2.waitKey(1)
 
-cv2.destroyAllWindows(0)
+cv2.destroyAllWindows(1)
