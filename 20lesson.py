8a7bdc1e9f2e6ca6cf0be11bba382f2b1f8cbd3b
Victor <vbivol394@gmail.com>
2026-02-12 23:24:07 +0200

    update

---
+++ 20lesson.py
@@ -0,0 +1,11 @@
+import cv2
+from datetime import datetime as dt
+import serial
+
+# victor = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+
+# face = cv2.CascadeClassifier(
+#     cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
+
+
+print("asa \n  asasa '")
